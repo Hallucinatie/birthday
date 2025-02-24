b35e5d99648ca45d8eeb56505094b37f50e7f074
0722Jessica <2382582769@qq.com>
2025-02-24 22:52:21 +0800

    【upd】planDate Warning mqh

--- src/App.vue
+++ src/App.vue
@@ -60,6 +60,12 @@
               <el-icon><Bell /></el-icon>
               距离下次生日还有 <span class="highlight">{{ daysUntilNextBirthday }}</span> 天
             </div>
+            <!-- mqh -->
+            <div v-if=" isBirthDateBeforeToday" class="info-item adjust-notice">
+                  <el-icon><Warning /></el-icon>
+                  <span>出生晚于今天！系统将仅默认您选择的出生月日</span>
+            </div>
+            <!-- end -->
           </Transition>
           <el-button type="primary" @click="nextStep" :disabled="!birthDate || !today">
             <el-icon class="button-icon"><ArrowRight /></el-icon>
@@ -408,7 +414,11 @@ const isPlanDateAfterBirthday = computed(() => {
   if (!planDate.value || !nextBirthdayDate.value) return false
   return dayjs(planDate.value).isAfter(dayjs(nextBirthdayDate.value))
 })
-
+// 检查出生日期是否在当前日期之前
+const isBirthDateBeforeToday = computed(() => {
+  if (!birthDate.value) return false
+  return dayjs(birthDate.value).isBefore(dayjs(today.value))
+})
 // 检查所有可能的计划日期是否都已过期
 const allPossibleDatesExpired = computed(() => {
   if (!originalPlanDate.value || !nextBirthdayDate.value) return false
